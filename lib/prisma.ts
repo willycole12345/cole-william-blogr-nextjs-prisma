d8338a8dbf6cc2e590538ce6ce26a60c018e6827
william cole <cole.williams043@gmail.com>
2021-12-30 18:26:11 +0100

    first commit

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!prisma) {
+    prisma = new PrismaClient();
+  }
+  prisma = prisma;
+}
+
+export default prisma;
+
